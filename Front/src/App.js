bf6cb22b3ead12ec968778623565c162fcd478be
Maxime <maxime.michel03@protonmail.com>
2021-10-15 16:54:52 +0200

    Add Empty input and fixe front click delete/check

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,46 +1,53 @@
 
 import './App.css';
 // eslint-disable-next-line
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const[newTodo, setToDo] = useState("")
-  const[data, setData] = useState([])
+  const [newTodo, setToDo] = useState("")
+  const [data, setData] = useState([])
 
   useEffect(() => {
     fetch("/todolist")
-    .then(res => res.json())
-    .then(data => setData(data))
+      .then(res => res.json())
+      .then(data => setData(data))
   }, [])
 
-  let AddMessage = async () => {     
-    const rawResponse = await fetch('/post', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({id: data.length+1, msg: newTodo, checked: false})
-    });
-
-    const content = await rawResponse.json();
-
-    setData(content)
+  // eslint-disable-next-line
+  let AddMessage = async () => {
+    if (document.getElementById("myInput").value !== "") {
+      const rawResponse = await fetch('/post', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: data.length + 1, msg: newTodo, checked: false })
+      });
+      const content = await rawResponse.json();
+      setData(content)
+      Clear()
+    }
   }
+
+  let Clear = () => {
+    setToDo("")
+    document.getElementById("myInput").value = ""
+  }
+
 
   let Close = async (id) => {
     fetch('/delete/' + id, {
       method: 'DELETE'
     })
-    .then(res => res.json())
-    .then(data => setData(data))
+      .then(res => res.json())
+      .then(data => setData(data))
   }
 
-  let Checked = async (id) => {     
+  let Checked = async (id) => {
     const rawResponse = await fetch('/checked/' + id, {
       method: 'PUT',
     });
-
     const content = await rawResponse.json();
 
     setData(content)
@@ -55,14 +62,17 @@ function App() {
       </div>
 
       <ul id="MyUl">
-      {data.map(function (todo, key) {
-        return(
-          <li key={todo.id} className={todo.checked ? "checked" : ""} onClick={event => Checked(todo.id)}>
-            {todo.msg}
-            <span id={todo.id} className="close" onClick={event => Close(event.target.id)}>{'\u00D7'}</span>
-          </li>
-        )
-      })}
+        {data.map(function (todo, key) {
+          return (
+            <li key={todo.id} className={todo.checked ? "checked" : ""} >
+              <div className={todo.checked ? "checked" : ""} onClick={() => Checked(todo.id)}>
+                {todo.msg}
+              </div>
+              <span id={todo.id} className="close" onClick={event => Close(event.target.id)}>{'\u00D7'}</span>
+            </li>
+
+          )
+        })}
       </ul>
     </div>
   );
